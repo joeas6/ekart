8d56d99d34b40f0467bc0adbdf265b578f552e2c
Joe <joeas6@yahoo.com>
2020-05-19 21:29:00 +0530

    tml Commit

--- node-app/db.js
+++ node-app/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-var dbHost = "mongodb://localhost:27017/ekart";
+var dbHost = "mongodb://mongodb:27017/ekart";
 
 mongoose.connect(dbHost, (err) => {
     if (err) {
